7b329b923a04ef993bb1f52839f8fdf3fa600349
Ryan Haining <haining.cpp@gmail.com>
2023-12-18 20:31:04 -0800

    Much simpler version of accounting for the extra border.
    Obvious once I saw someone describe it. Kinda embarrassed I didn't
    realize this would work.

--- day18/Day18P1.kt
+++ day18/Day18P1.kt
@@ -2,12 +2,6 @@ import kotlin.math.abs
 
 private data class Point(val row: Int, val col: Int)
 
-private data class MutablePoint(var row: Int, var col: Int) {
-  companion object {
-    fun from(p: Point) = MutablePoint(p.row, p.col)
-  }
-}
-
 private enum class Direction {
   UP {
     override fun adjust(point: Point, distance: Int) = point.copy(row=point.row-distance)
@@ -36,51 +30,23 @@ private enum class Direction {
 
 fun main() {
   val points = mutableListOf<Point>(Point(0, 0))
+  var length = 0
   generateSequence(::readLine).forEach { line->
     val (dirStr, distanceStr, _) = line.split(" ")
     require(dirStr.length == 1) { "got mult-char direction: $dirStr" }
     val direction = Direction.from(dirStr[0])
     val distance = distanceStr.toInt()
+    length += distance
     points.add(direction.adjust(points.last(), distance))
   }
   points.removeLast()
 
-  // Because the points represent the holes themselves, not the edges of the holes,
-  // the area will be using the point as the outside edge on one side, but the
-  // inside edge on the other. To compensate, we push the borders out when moving
-  // down or left.
-  // We need to make sure we are starting on the left side of the rectangle for this
-  // specific version of the expansion to work
-  val minCol = points.minBy {it.col }.col
-  val leftSide = points.filter { it.col == minCol }
-  val start = leftSide.minBy { it.row }
-  val startIndex = points.indexOf(start)
-  val adjustedPoints = (
-      points.slice(startIndex..<points.size) +
-      points.slice(0..<startIndex)
-  ).map(MutablePoint::from)
-  for ((p1, p2) in adjustedPoints.zipWithNext()) {
-    check (p1.row == p2.row || p1.col == p2.col) {
-      "Points are not in same row or same column: $p1, $p2"
-    }
-    if (p1.row < p2.row) {
-      // moving down
-      ++p1.col
-      ++p2.col
-    } else if (p2.col < p1.col) {
-      // moving left
-      ++p1.row
-      ++p2.row
-    } else {
-      check (p1.row > p2.row || p2.col > p1.col) {
-        "should be moving up or right but are not $p1 $p2"
-      }
-    }
-  }
-
   // shoelace formula
-  val part1 = adjustedPoints.zipWithNext().map {(p1, p2) -> p1.row * p2.col}.sum() + adjustedPoints.last().row*adjustedPoints.first().col
-  val part2 = adjustedPoints.zipWithNext().map {(p1, p2) -> p1.col * p2.row}.sum() + adjustedPoints.last().col*adjustedPoints.first().row
-  val area = abs(part1-part2)/2
+  val part1 = points.zipWithNext().map {(p1, p2) -> p1.row * p2.col}.sum() + points.last().row*points.first().col
+  val part2 = points.zipWithNext().map {(p1, p2) -> p1.col * p2.row}.sum() + points.last().col*points.first().row
+  // Only the half the border is counted, you can think of the points as being the top-left
+  // corner of the hole. This means we need to add half the perimeter, then an additional
+  // +1 for the 4 outer corners that don't have corresponding inner corners.
+  val area = (abs(part1-part2)/2) + (length/2) + 1
   println(area)
 }
--- day18/Day18P2.kt
+++ day18/Day18P2.kt
@@ -2,12 +2,6 @@ import kotlin.math.abs
 
 private data class Point(val row: Long, val col: Long)
 
-private data class MutablePoint(var row: Long, var col: Long) {
-  companion object {
-    fun from(p: Point) = MutablePoint(p.row, p.col)
-  }
-}
-
 private enum class Direction {
   UP {
     override fun adjust(point: Point, distance: Long) = point.copy(row=point.row-distance)
@@ -36,50 +30,22 @@ private enum class Direction {
 
 fun main() {
   val points = mutableListOf<Point>(Point(0, 0))
+  var length = 0L
   generateSequence(::readLine).forEach { line->
     val encoded = line.split("(#")[1].trimEnd(')')
     val direction = Direction.from(encoded.last())
     val distance = encoded.slice(0..<encoded.length-1).toLong(radix=16)
+    length += distance
     points.add(direction.adjust(points.last(), distance))
   }
   points.removeLast()
 
-  // Because the points represent the holes themselves, not the edges of the holes,
-  // the area will be using the point as the outside edge on one side, but the
-  // inside edge on the other. To compensate, we push the borders out when moving
-  // down or left.
-  // We need to make sure we are starting on the left side of the rectangle for this
-  // specific version of the expansion to work
-  val minCol = points.minBy {it.col }.col
-  val leftSide = points.filter { it.col == minCol }
-  val start = leftSide.minBy { it.row }
-  val startIndex = points.indexOf(start)
-  val adjustedPoints = (
-      points.slice(startIndex..<points.size) +
-      points.slice(0..<startIndex)
-  ).map(MutablePoint::from)
-  for ((p1, p2) in adjustedPoints.zipWithNext()) {
-    check (p1.row == p2.row || p1.col == p2.col) {
-      "Points are not in same row or same column: $p1, $p2"
-    }
-    if (p1.row < p2.row) {
-      // moving down
-      ++p1.col
-      ++p2.col
-    } else if (p2.col < p1.col) {
-      // moving left
-      ++p1.row
-      ++p2.row
-    } else {
-      check (p1.row > p2.row || p2.col > p1.col) {
-        "should be moving up or right but are not $p1 $p2"
-      }
-    }
-  }
-
   // shoelace formula
-  val part1 = adjustedPoints.zipWithNext().map {(p1, p2) -> p1.row * p2.col}.sum() + adjustedPoints.last().row*adjustedPoints.first().col
-  val part2 = adjustedPoints.zipWithNext().map {(p1, p2) -> p1.col * p2.row}.sum() + adjustedPoints.last().col*adjustedPoints.first().row
-  val area = abs(part1-part2)/2
+  val part1 = points.zipWithNext().map {(p1, p2) -> p1.row * p2.col}.sum() + points.last().row*points.first().col
+  val part2 = points.zipWithNext().map {(p1, p2) -> p1.col * p2.row}.sum() + points.last().col*points.first().row
+  // Only the half the border is counted, you can think of the points as being the top-left
+  // corner of the hole. This means we need to add half the perimeter, then an additional
+  // +1 for the 4 outer corners that don't have corresponding inner corners.
+  val area = (abs(part1-part2)/2) + (length/2) + 1
   println(area)
 }
